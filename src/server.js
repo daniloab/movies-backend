f27f9bf499b40be5afc1ab4c4daf99c1a680bc5d
Danilo Assis <daniloassis.ti@gmail.com>
2019-05-24 15:44:43 -0300

    Set PORT

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import app from './graphql/app'
 (async () => {
     const server = createServer(app.callback())
 
-    server.listen(process.env.GRAPHQL_PORT, () => {        
+    server.listen(process.env.GRAPHQL_PORT || 9001, () => {        
         console.log(`App running and listening on ${process.env.URL} ...`);
     })
 })()
